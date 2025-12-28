6b8a7c4c5552308ec9279a3b39f7bb4ce2ee90af
callmenoob77 <gabrieldima6543@gmail.com>
2025-12-28 15:56:46 +0200

    First push

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+import time
+import random
+
+WIDTH = 1000
+HEIGHT = 800
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bubble Buster")
+
+BG = pygame.image.load("bkgr.jpg")
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+        draw()
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
